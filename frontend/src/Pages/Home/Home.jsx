39d4802504c8edf0d6a02852b57d4e6393d2d956
Shruthi <shruthitech29@gmail.com>
2026-06-12 10:45:43 +0530

    WIP: added contact section setup

--- frontend/src/Pages/Home/Home.jsx
+++ frontend/src/Pages/Home/Home.jsx
@@ -1,5 +1,20 @@
+import Hero from "../../components/hero/Hero";
+import ProductOverview from "../../components/features/ProductOverview";
+import FeaturesSection from "../../components/features/FeaturesSection";
+import ContactSection from "../../components/contact/ContactSection";
+
 function Home() {
-  return <h1>Home Page</h1>;
+  return (
+    <>
+      <Hero />
+
+      <ProductOverview />
+
+      <FeaturesSection />
+
+      <ContactSection />
+    </>
+  );
 }
 
 export default Home;
